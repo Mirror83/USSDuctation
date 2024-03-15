57fc723fc3d3ea769603dc759272d60a3e2d36fd
Rontim <rongitonga@gmail.com>
2024-03-15 13:04:12 +0300

    Feat(Model): Added models and registered themin the admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['reg_no', 'name', 'course',
+                    'current_year', 'current_semester']
+    search_fields = ['reg_no', 'name', 'course']
+    list_filter = ['current_year', 'current_semester']
+
+
+@admin.register(Unit_Info)
+class UnitInfoAdmin(admin.ModelAdmin):
+    list_display = ['unit_code', 'unit_name', 'year', 'semester', 'course']
+    search_fields = ['unit_code', 'unit_name', 'course']
+    list_filter = ['year', 'semester']
+
+
+@admin.register(Student_Units)
+class StudentUnitsAdmin(admin.ModelAdmin):
+    list_display = ['reg_no', 'unit_code', 'result']
+    search_fields = ['reg_no', 'unit_code']
+    list_filter = ['result']
+
+
+@admin.register(Student_Finance)
+class StudentFinanceAdmin(admin.ModelAdmin):
+    list_display = ['reg_no', 'balance', 'fee', 'date']
+    search_fields = ['reg_no', 'balance', 'fee']
+    list_filter = ['date']
--- app/models.py
+++ app/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    reg_no = models.CharField(max_length=10, unique=True, primary_key=True)
+    name = models.CharField(max_length=100)
+    course = models.CharField(max_length=100)
+    current_year = models.IntegerField()
+    current_semester = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.reg_no} - {self.name} - {self.course} - {self.current_year} - {self.current_semester}'
+
+    class Meta:
+        db_table = 'student'
+
+
+class Unit_Info(models.Model):
+    unit_code = models.CharField(max_length=10, unique=True, primary_key=True)
+    unit_name = models.CharField(max_length=100)
+    year = models.IntegerField()
+    semester = models.IntegerField()
+    course = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.unit_code} - {self.unit_name} - {self.year} - {self.semester} - {self.course}'
+
+    class Meta:
+        db_table = 'unit_info'
+
+
+class Student_Units(models.Model):
+    reg_no = models.ForeignKey(Student, on_delete=models.CASCADE)
+    unit_code = models.ForeignKey(Unit_Info, on_delete=models.CASCADE)
+    result = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f'{self.reg_no} - {self.unit_code}'
+
+    class Meta:
+        db_table = 'student_units'
+
+
+class Student_Finance(models.Model):
+    reg_no = models.ForeignKey(Student, on_delete=models.CASCADE)
+    balance = models.FloatField()
+    fee = models.FloatField()
+    date = models.DateField()
+
+    def __str__(self):
+        return f'{self.reg_no} - {self.amount} - {self.date}'
+
+    class Meta:
+        db_table = 'student_finance'
